c24ad0c2858632b037d73fd072cd293c5110f7a6
Derek Popich <djpopich@gmail.com>
2022-10-12 14:19:15 -0600

    Fixed Link error

--- BACKEND/backendMain.cpp
+++ BACKEND/backendMain.cpp
@@ -3,3 +3,10 @@
 * Authors: Derek Popich, Cannon Palmer, Brian Trybus, Alfredo Restrepo
 * Purpose: This is the main file for the Orbital Trajectory Estimation Software backend.
 */
+
+#include <iostream>
+
+int main() 
+{
+	return 0;
+}

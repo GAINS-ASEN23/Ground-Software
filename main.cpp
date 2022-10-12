2c84e64b92b0b0364c25b23ebb185c73691062ba
Derek Popich <djpopich@gmail.com>
2022-10-12 14:47:47 -0600

    Added Header File

--- main.cpp
+++ main.cpp
@@ -8,36 +8,12 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <main.h>
 
 
 // Create functions to outline what happens in window
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
-// Assign window height and width
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-
-const char* fragmentShader2Source = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.8f, 0.0f, 1.0f);\n"
-"}\n\0";
 
 int main()
 {
@@ -86,6 +62,7 @@ int main()
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
+
     // check for shader compile errors
     int success;
     char infoLog[512];
---
+++ main.h
@@ -0,0 +1,31 @@
+#pragma once
+/* MAIN FRONTEND HEADER FILE
+* GAINS Senior Project Capstone Ground Software GUI Frontend
+* Authors: Derek Popich, Cannon Palmer, Brian Trybus, Alfredo Restrepo
+* Purpose: This is the main header file for the Orbital Trajectory Estimation Software
+*/
+
+// Assign window height and width
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+const char* fragmentShader2Source = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.8f, 0.0f, 1.0f);\n"
+"}\n\0";

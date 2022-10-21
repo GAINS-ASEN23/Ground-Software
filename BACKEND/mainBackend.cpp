c6c37b72596b3b19c50d26aa38a87b99836707fa
djpopich <djpopich@gmail.com>
2022-10-20 20:45:45 -0600

    redo of BACKEND

--- BACKEND/mainBackend.cpp
+++ BACKEND/mainBackend.cpp
@@ -1,17 +1,12 @@
-/* MAIN BACKEND FILE 
+/* MAIN BACKEND FILE
 * GAINS Senior Project Capstone Ground Software Backend
 * Authors: Derek Popich, Cannon Palmer, Brian Trybus, Alfredo Restrepo
 * Purpose: This is the main file for the Orbital Trajectory Estimation Software backend.
 */
 
 #include <iostream>
-#include <cppSpice/spice_label_map.hpp>
-#include <cppSpice/spice_kernel_set.hpp>
-#include <cppSpice/spice_time.hpp>
-#include <cppSpice/spice_ephemeris.hpp>
-#include <cppSpice/spice_error.hpp>
 
-int main() 
+int main()
 {
-	return 0;
+    return 0;
 }

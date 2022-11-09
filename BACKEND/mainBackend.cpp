cd21eebf6dc26348fb9bac1e8292f822938e4ef7
Derek Popich <djpopich@gmail.com>
2022-11-09 14:23:25 -0700

    Eigen 3.4.0 Implemented

--- BACKEND/mainBackend.cpp
+++ BACKEND/mainBackend.cpp
@@ -5,8 +5,11 @@
 */
 
 #include <iostream>
+#include "..\Libraries\include\Eigen\Dense"
 
 #include "mainBackend.h"
+
+using Eigen::MatrixXd;
 
 int main()
 {
@@ -14,6 +17,13 @@ int main()
     
     // Test Function from sub files 
     spiceOBJ.printSpiceData();
+
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << m << std::endl;
 
     return 0;
 }
